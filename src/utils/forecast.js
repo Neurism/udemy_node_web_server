4386e17916aa6c0de5a2680765516cd095a675ff
Jess Farmer <farmerj2013@my.fit.edu>
2019-10-16 22:15:48 -0400

    added more data to forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The pressure is currently ' + body.currently.pressure + '. The high and low for today are ' + body.daily.data[0].temperatureHigh + ', ' + body.daily.data[0].temperatureLow + '.');
         }
     })
 }
